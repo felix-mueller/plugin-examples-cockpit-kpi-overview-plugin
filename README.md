882c030753316df984f1f3d1274d1d15bc2fc87a
Felix Mueller <felix.mueller@camunda.com>
2016-08-09 17:36:39 +0200

    Updating read

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 
 ## ToDos
 
-- Pagination (improve code for many instances)
-- 
-
-## Related Concepts
-
+- Filtering on time frames
+- Send reminder emails
 
 ## What's the point?
+With the help of this Cockpit Plugin one can display KPIs that were defined within the BPMN model using the KPI Element Template provided within this repository. 
+This plugin provides two views that integrate into two extension points:
+###Process Definition History Tab
 
+###Process Instance History Tab
 
 ## How to register a Cockpit plugin?
 
+> ###Development purposes###
+For development purposes it is best to run the cockpit plugin inside a standalone Camunda EE.
+This is already set up and one can go ahead by using Maven  mvn jetty:run -P develop
 
-## Integrate into Camunda webapp
+----------
 
+> ###Production purposes###
+For production purposes it is best to package the cockpit plugin using mvn package and then copy it to Camunda WebApp Lib folder.
 
 ## License
 
--- src/main/java/org/camunda/bpm/cockpit/plugin/kpiOverview/resources/KPIStatisticsResource.java
+++ src/main/java/org/camunda/bpm/cockpit/plugin/kpiOverview/resources/KPIStatisticsResource.java
@@ -21,49 +21,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class KPIStatisticsResource extends AbstractCockpitPluginResource {
 	private String statisticData;
 	private String processDefinitionId;
-	
+
 	public KPIStatisticsResource(String engineName, String processDefinitionId, String statisticData) {
 		super(engineName);
 		this.processDefinitionId = processDefinitionId;
 		this.statisticData = statisticData;
 	}
-	
-	  @GET
-	  public List<TaskStatisticHistoryCountDto> getTaskStatisticHistoryResource() {
-	    HashMap<String, String> parameters = new HashMap<String, String>();
-	    parameters.put("processDefinitionId", processDefinitionId);
-	    
-	    ObjectMapper mapper = new ObjectMapper();
-	    try {
+
+	@GET
+	public List<TaskStatisticHistoryCountDto> getTaskStatisticHistoryResource() {
+		HashMap<String, String> parameters = new HashMap<String, String>();
+		parameters.put("processDefinitionId", processDefinitionId);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
 			List<StatisticData> data = mapper.readValue(statisticData, new TypeReference<List<StatisticData>>() { });
 			List<TaskStatisticHistoryCountDto> results = new ArrayList<TaskStatisticHistoryCountDto>();
-			
+
 			for(int i = 0; i < data.size(); i++) {
-				
+
 				StatisticData statisticData = data.get(i);
 				if (statisticData.getKpi() != null && !statisticData.getKpi().isEmpty()) {
 					TaskStatisticHistoryCountDto countDto = new TaskStatisticHistoryCountDto();
 					countDto.setActivityId(statisticData.getTaskId());
 					parameters.put("statisticDataUnit", statisticData.getKpiUnit());
 					parameters.put("taskId", statisticData.getTaskId());
-				    QueryParameters<TaskStatisticHistoryDto> queryParameters = new QueryParameters<TaskStatisticHistoryDto>();
-				    queryParameters.setParameter(parameters);
-				    List<TaskStatisticHistoryDto> result = getQueryService().executeQuery("cockpit.kpi.selectTaskStatisticHistory", queryParameters);
-				    
-				    if (result.size() > 0) {
-				    	int count = 0;
-				    	for (TaskStatisticHistoryDto dto : result) {
-				    		if (dto.getDiff() > Integer.parseInt(statisticData.getKpi())) {
-				    			count++;
-				    		}
-				    	}	
-				    	countDto.setCount(count);
-				    }
-				    results.add(countDto);
+					QueryParameters<TaskStatisticHistoryDto> queryParameters = new QueryParameters<TaskStatisticHistoryDto>();
+					queryParameters.setParameter(parameters);
+					List<TaskStatisticHistoryDto> result = getQueryService().executeQuery("cockpit.kpi.selectTaskStatisticHistory", queryParameters);
+
+					if (result.size() > 0) {
+						int count = 0;
+						for (TaskStatisticHistoryDto dto : result) {
+							if (dto.getDiff() > Integer.parseInt(statisticData.getKpi())) {
+								count++;
+							}
+						}	
+						countDto.setCount(count);
+					}
+					results.add(countDto);
 				}
 			}
-
-		    return results;
+			return results;
 		} catch (JsonParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,8 +76,6 @@ public class KPIStatisticsResource extends AbstractCockpitPluginResource {
 			e.printStackTrace();
 			return null;
 		}
-
-	   
-	  }
-
+	}
+	
 }
